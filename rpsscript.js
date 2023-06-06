09fcac627ce20f59ff546f0e97013d89a79b8de6
James S <98997059+jamesfsher@users.noreply.github.com>
2023-06-05 22:06:17 -0500

    first working beta

---
+++ rpsscript.js
@@ -0,0 +1,93 @@
+function game() {
+    // Initialize scores
+    let compScore = 0;
+    let userScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        console.log("Round " + (i + 1));
+        // Generate computer choice
+        let computerSelection = computerSelectionGenerator();
+
+        // Prompts user for their choice
+        let playerSelection = prompt("Enter rock, paper, or scissors");
+        // Capitalizes user's choice for comparison
+        playerSelection = playerSelection.toUpperCase();
+
+        // Loop to make sure user has selected valid response, and conintues to prompt until valid response is entered
+        while (playerSelection != "ROCK" && playerSelection != "PAPER" && playerSelection != "SCISSORS") {
+            playerSelection = prompt("Enter valid response of rock, paper, or scissors");
+            playerSelection = playerSelection.toUpperCase();
+        }
+        console.log("user choice is " + playerSelection)
+        console.log("computer choice is " + computerSelection);
+
+        // Gets result of one round of RPS
+        let result = playRound(playerSelection, computerSelection);
+
+        // Uses result of playRound function to tally of scores
+        if (result == "compWin") {
+            compScore += 1;
+        }
+        else if (result == "userWin") {
+            userScore += 1;
+        }
+
+        console.log("Computer: " + compScore);
+        console.log("User: " + userScore);
+
+        console.log("");
+    }
+    console.log("GAME OVER");
+    console.log("Computer: " + compScore);
+    console.log("User: " + userScore);
+}
+
+// Function that generates randomly computer choice
+function computerSelectionGenerator() {
+    // Array of choices for Computer
+    let choices = ["ROCK", "PAPER", "SCISSORS"];
+    // randomly selects index based on length of choices array
+    var choice = choices[Math.floor(Math.random()*choices.length)];
+    return choice;
+}
+
+// Function to play a round of RPS, returns winner as either compWin or userWin as a string
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("TIE");
+        return "TIE";
+    }
+    else if (playerSelection == "ROCK") {
+        if (computerSelection == "PAPER"){
+            console.log("computer wins, paper beats rock");
+            return "compWin";
+        }
+        if (computerSelection == "SCISSORS") {
+            console.log("you win, rock beats scissors");
+            return "userWin";
+        }
+    }
+    else if (playerSelection == "PAPER") {
+        if (computerSelection == "ROCK") {
+            console.log("you win, paper beats rock");
+            return "userWin";
+        }
+        else {
+            console.log("computer wins, scissors beats rock");
+            return "compWin";
+        }
+    }
+    else {
+        if (computerSelection == "ROCK") {
+            console.log("computer wins, rock beats scissors");
+            return "compWin";
+        }
+        else {
+            console.log("you win, scissors beats paper");
+            return "userWin";
+        }
+    }
+}
+
+
+game();
